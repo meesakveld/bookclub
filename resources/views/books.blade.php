c369de4bb5f8f67a8184469e13d49981e83e7bd4
meesakveld <meesakveld@gmail.com>
2025-01-08 16:20:24 +0100

    Refactored book display layout in books.blade.php for improved aesthetics and clarity

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -12,16 +12,13 @@
                 <p>On this page you will find all books that are available.</p>
             </div>
 
-            <div>
+            <div class="my-4 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 @foreach ($books as $book)
-                    <div
-                        class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4 flex justify-between items-center p-6">
-                        <div class="text-gray-900">
-                        <img src="{{ asset('storage/' . $book->image_url) }}" alt="">
-                            <h2 class="text-xl font-semibold">{{ $book->title }}</h2>
-                            <p>{{ $book->description }}</p>
-                            <p>{{ $book->published_at }}</p>
-                        </div>
+                    <div class="bg-white p-6 flex flex-col gap-4 rounded-lg">
+                        <img src="{{ asset('storage/' . $book->image_url) }}" alt="{{ $book->title }}" class="w-full rounded-lg">
+                        <h2 class="text-xl font-semibold">{{ $book->title }}</h2>
+                        <p>{{ $book->description }}</p>
+                        <p class="text-gray-500">Publishing date: {{ $book->published_at }}</p>
                     </div>
                 @endforeach
             </div>
