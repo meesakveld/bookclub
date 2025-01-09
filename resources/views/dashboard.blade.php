ed60964e2074f3b3e7016adbf46470a8041fc9b6
meesakveld <meesakveld@gmail.com>
2025-01-09 09:05:00 +0100

    Enhanced dashboard layout with a welcome message and improved navigation links for bookclubs and books

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,13 +5,36 @@
         </h1>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="py-12 max-w-7xl mx-auto p-6 flex flex-col gap-12">
+        <div>
+            <h2 class="text-2xl font-semibold">Welkom!</h2>
+            <p>Magna ut ipsum cillum elit mollit dolor laborum pariatur eu ut adipisicing esse laborum. Fugiat aliquip
+                laborum consequat fugiat do excepteur. Sunt aute ad et fugiat amet minim in sunt aliqua minim ut culpa
+                mollit. Aliquip ad elit quis quis ex et non. Magna velit minim fugiat sunt nostrud laboris dolor. Anim
+                dolore cillum consectetur adipisicing deserunt occaecat veniam. Nisi veniam anim aliquip ea dolore
+                cillum tempor mollit cupidatat ipsum cupidatat.
+
+                Sint Lorem aliqua nostrud incididunt mollit consequat tempor non consectetur esse ullamco culpa.
+                Consectetur est aliqua culpa culpa. Elit eiusmod Lorem minim incididunt non nulla voluptate occaecat
+                quis duis id nisi irure. Esse velit et labore veniam elit velit ad dolor proident culpa pariatur.
+                Commodo Lorem minim aliquip cupidatat reprehenderit. Magna anim voluptate dolor exercitation minim
+                veniam velit nostrud ullamco deserunt ex officia anim. Enim amet sint do ex pariatur do.</p>
+        </div>
+
+        <div class=" grid grid-cols-1 md:grid-cols-2 gap-6">
+            <a href="{{ route('bookclubs') }}">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-full aspect-square flex flex-col items-center justify-center">
+                    <p class="text-green-800 font-bold text-4xl">Bookclubs</p>
                 </div>
-            </div>
+            </a>
+
+            <a href="{{ route('books') }}">
+                <div
+                    class="bg-white overflow-hidden shadow-sm sm:rounded-lg w-full aspect-square flex flex-col items-center justify-center">
+                    <p class="text-green-800 font-bold text-4xl">Books</p>
+                </div>
+            </a>
         </div>
     </div>
 </x-layouts.app>
