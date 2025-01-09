3d87f9060775bf0066a213a4fa3fc81bcf9a4a34
meesakveld <meesakveld@gmail.com>
2025-01-09 09:25:34 +0100

    Removed noindex meta tag to allow search engines to index the site

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -18,8 +18,6 @@
 
     <link rel="canonical" href="{{ url()->current() }}" />
 
-    <meta name="robots" content="noindex, nofollow" />
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
