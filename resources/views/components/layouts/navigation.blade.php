086c333a5e74b4859622f5700954072b2b7f66af
meesakveld <meesakveld@gmail.com>
2025-01-24 15:04:21 +0100

    Added padding to main content and new navigation links for Bookclubs and Books

--- resources/views/components/layouts/navigation.blade.php
+++ resources/views/components/layouts/navigation.blade.php
@@ -82,6 +82,14 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('bookclubs')" :active="request()->routeIs('bookclubs')">
+                {{ __('Bookclubs') }}
+            </x-responsive-nav-link>
+            
+            <x-responsive-nav-link :href="route('books')" :active="request()->routeIs('books')">
+                {{ __('Books') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
